d6e818a0b48786eeec464774985b0bb410cb61e8
Mahmoud <Ma7moudismail47@gmail.com>
2025-08-20 12:36:35 +0300

    Update message in HelloPy.py

--- HelloPy.py
+++ HelloPy.py
@@ -2,6 +2,12 @@ class HelloWorld:
     def printHelloWorld(self):
         print('Hello World')
 
+class HelloPy:
+    def printHelloWorld(self):
+        print("Hello Java World!")     
+
 if __name__ == '__main__':
     hw = HelloWorld()
     hw.printHelloWorld()
+    hj = HelloPy()
+    hj.printHelloWorld()  
