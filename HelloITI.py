b68618aa3a300f056fa056b00aaf9d03abbe5a6d
Mahmoud <Ma7moudismail47@gmail.com>
2025-08-20 12:29:32 +0300

    Add printHelloGit method to HelloITI.py

--- HelloITI.py
+++ HelloITI.py
@@ -1,7 +1,14 @@
+class HelloGit:
+    def printHelloGit(self):
+        print("Hello Git")
+
 class HelloITI:
     def printHelloITI(self):
         print("Hello ITI")
 
 if __name__ == "__main__":
-    hi = HelloITI()
-    hi.printHelloITI()
+    hi1 = HelloGit()
+    hi1.printHelloGit()
+    
+    hi2 = HelloITI()
+    hi2.printHelloITI()
